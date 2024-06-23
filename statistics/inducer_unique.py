79f454eb4208f73876f4af8001525b3ded4e192c
minato <yanggang.chen7@gmai.com>
2024-06-23 10:53:57 +0900

    fix

--- statistics/inducer_unique.py
+++ statistics/inducer_unique.py
@@ -25,18 +25,26 @@ unique_inducers = df[['L_inducer', 'a_inducer', 'b_inducer']].drop_duplicates().
 unique_sizes = df['size'].drop_duplicates().values
 os.makedirs('inducer_unique_plots', exist_ok=True)
 
+
+# 定义随机偏移量函数
+def add_random_jitter(arr, jitter_strength=0.5):
+    return arr + np.random.normal(scale=jitter_strength, size=arr.shape)
+
+
 for inducer in unique_inducers:
     mask = (df[['L_inducer', 'a_inducer', 'b_inducer']] == inducer).all(axis=1)
     size_mask = df['size'] == df[mask]['size'].values[0]
     bar_colors_masked = bar_colors[mask & size_mask]
     plt.figure(figsize=(10, 5))
 
-    # 绘制label颜色
-    plt.scatter(df['a_label_mean'][mask], df['b_label_mean'][mask], c='black', marker='o', label='Labeled Colors',
-                linewidth=0.5)
+    # 绘制label颜色，添加随机偏移量
+    a_label_jittered = add_random_jitter(df['a_label_mean'][mask])
+    b_label_jittered = add_random_jitter(df['b_label_mean'][mask])
+    plt.scatter(a_label_jittered, b_label_jittered, c='black', marker='o', label='Labeled Colors', linewidth=0.5,
+                alpha=0.7)
 
-    # 绘制inducer颜色和test_color
-    plt.scatter(inducer[1], inducer[2], c=bar_colors_masked, s=70, label='Inducer Color')
+    # 绘制inducer颜色
+    plt.scatter(inducer[1], inducer[2], c=bar_colors_masked, marker='x', s=70, label='Inducer Color')
 
     # 绘制test_color,参数单个颜色用color，多个颜色用c
     plt.scatter(test_color[1], test_color[2], color=test_rgb, marker='o', s=70, label='Test Color')
@@ -46,7 +54,7 @@ for inducer in unique_inducers:
 
     # 标注size并优化文本标签位置避免重叠
     texts = []
-    for i, (a, b, size) in enumerate(zip(df['a_label_mean'][mask], df['b_label_mean'][mask], df['size'][mask])):
+    for i, (a, b, size) in enumerate(zip(a_label_jittered, b_label_jittered, df['size'][mask])):
         if '.' in str(size):
             label = f'{size:.1f}'
         else:
@@ -57,6 +65,7 @@ for inducer in unique_inducers:
 
     plt.xlabel("a")
     plt.ylabel("b")
+    plt.axis('equal')  # 确保横纵坐标刻度宽度相同
     inducer_int = tuple(int(x) for x in inducer)
     plt.title(f"For inducer {inducer_int}(or {int(hue_inducer[mask][0])}°) with different sizes")
     plt.legend()
@@ -65,5 +74,6 @@ for inducer in unique_inducers:
     plt.grid(True)
 
     plt.tight_layout()
-    plt.savefig(f'inducer_unique_plots/inducer_{inducer[0]}_{inducer[1]}_{inducer[2]}.png')
+    plt.savefig(
+        f'inducer_unique_plots/inducer_{inducer[0]}_{inducer[1]}_{inducer[2]}_hue_{int(hue_inducer[mask][0])}.png')
     plt.close()
